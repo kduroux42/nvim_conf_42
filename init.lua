afdce3a5156502d9e4405b978075b7a34de3b546
kduroux42 <163676522+kduroux42@users.noreply.github.com>
2024-10-14 14:46:20 +0200

    Add files via upload

--- init.lua
+++ init.lua
@@ -1,6 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+require('telescope').setup({
+	defaults = {
+		file_ignore_patterns = { '^obj/', },
+	}
+})
+
+require('hardtime').setup()
+
 
 local set = vim.opt
 
@@ -9,3 +17,6 @@ set.tabstop = TAB_WIDTH
 set.shiftwidth = TAB_WIDTH
 set.expandtab = false
 vim.g.autoformat = false
+--vim.cmd('colorscheme base16-tokyo-city-terminal-dark')
+--vim.cmd('colorscheme base16-summerfruit-dark')
+
--- lua/plugins/allplug.lua
+++ lua/plugins/allplug.lua
@@ -20,6 +20,37 @@ return {
 
 	{ "nyoom-engineering/oxocarbon.nvim" },
 
+	{ "RRethy/base16-nvim" },
+
+	{ "hougesen/blame-me.nvim" },
+
+	{
+		"m4xshen/hardtime.nvim",
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+
+	{
+		"kdheepak/lazygit.nvim",
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+		},
+	},
+
 	{
 		"LazyVim/LazyVim",
 		opts = {
